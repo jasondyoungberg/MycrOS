2da084388d0a41896ebfdae9662b9a52c6d4e4b7
Jason Youngberg <jasondyoungberg@gmail.com>
2024-06-14 16:59:23 -0600

    Allow runtime changes to idt

--- kernel/src/idt.rs
+++ kernel/src/idt.rs
@@ -1,18 +1,31 @@
+use core::pin::Pin;
+
 use alloc::boxed::Box;
+use spin::{Lazy, Mutex};
 use x86_64::{
     set_general_handler,
     structures::idt::{InterruptDescriptorTable, InterruptStackFrame},
 };
 
-pub fn init() {
-    let mut idt = Box::new(InterruptDescriptorTable::new());
+pub static IDT: Lazy<Mutex<Pin<Box<InterruptDescriptorTable>>>> = Lazy::new(|| {
+    let mut idt = InterruptDescriptorTable::new();
 
     set_general_handler!(&mut idt, general_handler);
 
-    Box::leak(idt).load();
+    Mutex::new(Box::pin(idt))
+});
+
+pub fn init() {
+    let idt = IDT.lock();
+    // Safety: This is safe since the IDT is in a static pinned box.
+    unsafe { idt.load_unsafe() };
 }
 
 #[allow(clippy::needless_pass_by_value)]
-fn general_handler(stack_frame: InterruptStackFrame, index: u8, error_code: Option<u64>) {
-    todo!("interrupt {index} ({error_code:?})\n{stack_frame:#?}");
+fn general_handler(_stack_frame: InterruptStackFrame, index: u8, error_code: Option<u64>) {
+    if let Some(error_code) = error_code {
+        log::warn!("interrupt {index} ({error_code})");
+    } else {
+        log::warn!("interrupt {index}");
+    }
 }
