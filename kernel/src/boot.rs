0d49361c6cb485957e91e30867c3f3ef53951bc2
Jason Youngberg <jasondyoungberg@gmail.com>
2024-07-07 14:24:21 -0600

    Reformat

--- kernel/src/boot.rs
+++ kernel/src/boot.rs
@@ -12,34 +12,30 @@ static BASE_REVISION: BaseRevision = BaseRevision::new();
 #[used]
 #[link_section = "requests"]
 static HHDM_REQUEST: HhdmRequest = HhdmRequest::new();
+pub static HHDM_RESPONSE: Lazy<&HhdmResponse> =
+    Lazy::new(|| HHDM_REQUEST.get_response().expect("HHDM request failed"));
 
 #[used]
 #[link_section = "requests"]
 static MEMORY_MAP_REQUEST: MemoryMapRequest = MemoryMapRequest::new();
-
-#[used]
-#[link_section = "requests"]
-pub static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new();
-
-#[used]
-#[link_section = "requests"]
-pub static SMP_REQUEST: SmpRequest = SmpRequest::new();
-
-pub static HHDM_RESPONSE: Lazy<&HhdmResponse> =
-    Lazy::new(|| HHDM_REQUEST.get_response().expect("HHDM request failed"));
-
 pub static MEMORY_MAP_RESPONSE: Lazy<&MemoryMapResponse> = Lazy::new(|| {
     MEMORY_MAP_REQUEST
         .get_response()
         .expect("Memory map request failed")
 });
 
+#[used]
+#[link_section = "requests"]
+pub static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new();
 pub static FRAMEBUFFER_RESPONSE: Lazy<&FramebufferResponse> = Lazy::new(|| {
     FRAMEBUFFER_REQUEST
         .get_response()
         .expect("Framebuffer request failed")
 });
 
+#[used]
+#[link_section = "requests"]
+pub static SMP_REQUEST: SmpRequest = SmpRequest::new();
 pub static SMP_RESPONSE: Lazy<&SmpResponse> =
     Lazy::new(|| SMP_REQUEST.get_response().expect("SMP request failed"));
 
